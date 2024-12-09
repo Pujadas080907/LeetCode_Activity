e45f0fa3fbe458dd5a36599cb6e916fc264168df
Puja Das <134757203+Pujadas080907@users.noreply.github.com>
2024-12-09 11:25:02 +0530

    Time: 0 ms (100.00%), Space: 10.7 MB (26.44%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -1,40 +1,27 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> v;
-        stack<TreeNode*> st;
-        TreeNode* node = root;
-        while(true)
-        {
-            if(node != NULL)
-            {
-                st.push(node);
-                node = node->left;
+        vector<int> inorder;
+        TreeNode* curr = root;
+        while (curr != NULL) {
+            if (curr->left == NULL) {
+                inorder.push_back(curr->val);
+                curr = curr->right;
+            } else {
+                TreeNode* prev = curr->left;
+                while (prev->right != NULL && prev->right != curr) {
+                    prev = prev->right;
+                }
+                if (prev->right == NULL) {
+                    prev->right = curr;
+                    curr = curr->left;
+                } else {
+                    prev->right = NULL;
+                    inorder.push_back(curr->val);
+                    curr = curr->right;
+                }
             }
-            else
-            if(st.empty() == true) 
-            {
-                break;
-            }else
-            {
-            node = st.top();
-            st.pop();
-            v.push_back(node->val);
-            node = node-> right;
-            }
-
         }
-        return v;
+        return inorder;
     }
 };
