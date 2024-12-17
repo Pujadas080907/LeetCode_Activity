a1b47eafcbc39fe0bda918d2ba344777d0308537
Puja Das <134757203+Pujadas080907@users.noreply.github.com>
2024-12-17 21:59:50 +0530

    Time: 0 ms (100.00%), Space: 18 MB (17.74%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,39 +1,44 @@
 class Solution {
 public:
-    int search(vector<int>& a, int k) {
-        int n = a.size();
-        int low = 0 , high = n-1;
-        while(low <= high)
-        {
-            int mid = (low + high)/2;
-            if(a[mid] == k) {
-                return true;
-            }
-            if(a[mid] == a[low] && a[mid]==a[high])
-            {
-                low ++; high --;
-                continue;
-            }
-            
-            if(a[low]<=a[mid])
-            {
-                if(a[low]<= k && k <= a[mid])
-                {
-                    high = mid - 1;
-                }else
-                {
-                    low = mid + 1;
-                }
-            }else
-            {
-                if(a[mid]<=k && k <= a[high]){
-                    low = mid + 1;
-                }else
-                {
-                    high = mid - 1;
-                }
-            }
-        }
-        return false;
+    bool search(vector<int>& arr, int target) {
+int low = 0; int high = arr.size() -1;
+while(low<=high)
+{
+int mid = (low + high) /2;
+if(arr[mid] == target)
+return true;
+if(arr[low] == arr[mid] && arr[mid] == arr[high])
+{
+    low = low +1;
+    high = high-1;
+    continue;
+}
+if(arr[low] <= arr[mid]) // left half sorted
+{
+
+if(arr[low]<= target && target <= arr[mid])
+{
+high =  mid -1;
+}else
+{
+low = mid+1;
+}
+
+}else
+{
+if(arr[mid]<=target && target <= arr[high])
+{
+low = mid + 1;
+}else
+{
+high = mid-1;
+}
+}
+
+
+}
+return false;
+   
     }
 };
+
