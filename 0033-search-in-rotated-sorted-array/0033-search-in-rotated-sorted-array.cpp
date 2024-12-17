24aa291d8ade3f16efe6964bc08dbff608d05cb1
Puja Das <134757203+Pujadas080907@users.noreply.github.com>
2024-12-17 14:00:02 +0530

    Time: 0 ms (100.00%), Space: 15.2 MB (20.54%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
-    int search(vector<int>& a, int k) {
-        int n = a.size();
-        int low = 0 , high = n-1;
-        while(low <= high)
-        {
-            int mid = (low + high)/2;
-            if(a[mid] == k) {
-                return mid;
-            }
-            
-            if(a[low]<=a[mid])
-            {
-                if(a[low]<= k && k <= a[mid])
-                {
-                    high = mid - 1;
-                }else
-                {
-                    low = mid + 1;
-                }
-            }else
-            {
-                if(a[mid]<=k && k <= a[high]){
-                    low = mid + 1;
-                }else
-                {
-                    high = mid - 1;
-                }
-            }
-        }
-        return -1;
+    int search(vector<int>& arr, int target) {
+        int low = 0; int high = arr.size() -1;
+while(low<=high)
+{
+int mid = (low + high) /2;
+if(arr[mid] == target)
+return mid;
+if(arr[low] <= arr[mid]) // left half sorted
+{
+
+if(arr[low]<= target && target <= arr[mid])
+{
+high =  mid -1;
+}else
+{
+low = mid+1;
+}
+
+}else
+{
+if(arr[mid]<=target && target <= arr[high])
+{
+low = mid + 1;
+}else
+{
+high = mid-1;
+}
+}
+
+
+}
+return -1;
+
     }
 };
