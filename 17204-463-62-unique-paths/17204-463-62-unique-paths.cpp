f3cbabaa7a024e8a2416d729910b45b5d6e016e7
Puja Das <134757203+Pujadas080907@users.noreply.github.com>
2025-02-21 21:06:52 +0530

    Time: 2 ms, Memory: 9.3 MB - LeetHub

---
+++ 17204-463-62-unique-paths/17204-463-62-unique-paths.cpp
@@ -0,0 +1,29 @@
+class Solution {
+
+    private:
+       int ways(int m, int n, vector<vector<int>> &dp){
+        
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                dp[0][0] = 1;
+                if(i == 0 && j == 0)
+                    continue;
+                int up =0, left = 0;
+
+                if(i>0) up = dp[i-1][j];
+                if(j>0) left = dp[i][j-1];
+                dp[i][j] = up + left;
+
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n,-1));
+        return ways(m, n,dp);
+    }
+};
