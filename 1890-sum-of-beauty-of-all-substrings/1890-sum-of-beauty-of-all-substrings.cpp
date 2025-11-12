ccf745196bfa1b752aef9a71a2c35dec3c7f05f5
Puja Das <134757203+Pujadas080907@users.noreply.github.com>
2025-11-13 00:51:23 +0530

    Time: 125 ms (29.03%), Space: 26.3 MB (12.82%) - LeetHub

---
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int beautySum(string s) {
+        int n = s.size();
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            unordered_map<char,int> fr;
+            for(int j=i; j<n; j++){
+                fr[s[j]]++;
+                int maxi = INT_MIN;
+                int mini = INT_MAX;
+                for(auto it : fr){
+                    maxi = max(maxi, it.second);
+                    mini = min(mini, it.second);
+
+                }
+                sum += (maxi - mini);
+            }
+        }
+        return sum;
+
+    }
+};
